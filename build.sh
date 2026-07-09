b4c09339d35e7232a065affc33f130cd69360949
AndreRH09 <aanazcoh@unsa.edu.pe>
2026-07-09 12:35:45 -0500

    config vercel 3

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 set -e
 
@@ -7,12 +6,10 @@ cd /tmp
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:/tmp/flutter/bin"
 
-echo "Configuring Flutter for web..."
 flutter config --enable-web
 flutter config --no-analytics
 
-echo "Going back to project..."
-cd $VERCEL_PROJECT_DIR
+cd /
 
 echo "Getting dependencies..."
 flutter pub get
@@ -20,4 +17,4 @@ flutter pub get
 echo "Building web..."
 flutter build web --release --no-tree-shake-icons
 
-echo "Build completed!"
+echo "✅ Build completed!"
